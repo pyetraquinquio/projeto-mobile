7d0d8dfb1b9d1fb37ca7ce0d7d3d6e7a05e0c933
Maria Eduarda <eduarda.maria140806@gmail.com>
2024-06-10 16:58:18 -0300

    8

--- backend/controller/salasController.js
+++ backend/controller/salasController.js
@@ -1,12 +1,14 @@
 const Sala = require('../model/salasModel');
 
 
-// Controlador para obter todos os SalasgetAllSalas
+// Controlador para obter todos os Produtos
 exports.getAllSalas = (req, res) => {
 Sala.getAllSalas((err, sala) => {
 if (err) {
 res.status(500).send(err);
+console.log("teste0")
 } else {
+console.log("teste1")
 res.json(sala);
 }
 });
--- backend/model/salasModel.js
+++ backend/model/salasModel.js
@@ -8,6 +8,7 @@ function openDbConnection() {
 let db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
 if (err) {
 console.error('Erro ao abrir o banco de dados:', err.message);
+console.log("teste2")
 }
 });
 return db;
@@ -19,9 +20,11 @@ function getAllSalas(callback) {
 const db = openDbConnection();
 db.all("SELECT * FROM sala", [], (err, rows) => {
 db.close();
+console.log("teste3")
 callback(err, rows);
 });
 }
+
 
 module.exports = {
 getAllSalas
